4e9bee12fcde6331cc6913b358740224c18a8d14
Darko Kukovec <darko@kukovec.eu>
2016-12-01 20:01:50 +0100

    Day 1 comments

--- tasks/1.js
+++ tasks/1.js
@@ -7,13 +7,17 @@ const {
 } = require('lodash/fp');
 
 module.exports = [
+
+  // Split the steps
   split(/,\s+/),
 
+  // Make the basic step preparation
   map((step) => ({
     direction: step[0] === 'L' ? 1 : -1,
     length: parseInt(step.slice(1), 10)
   })),
 
+  // Adjust the directions based on the previous direction
   reduce((prev, step) => [
     ...prev,
     {
@@ -25,18 +29,21 @@ module.exports = [
     length: 0
   }]),
 
+  // Make a list of moves
   map((step) => ([
-      [0, -step.length],
-      [-step.length, 0],
-      [0, step.length],
-      [step.length, 0]
+      [0, -step.length], // Left
+      [-step.length, 0], // Down
+      [0, step.length], // Right
+      [step.length, 0] // Up
     ])[step.direction]
   ),
 
+  // Sum up the steps
   reduce((sum, step) => [
     sum[0] + step[0],
     sum[1] + step[1]
   ], [0, 0]),
 
+  // Sum up the coordinates
   reduce((sum, coordinate) => sum + coordinate, 0)
 ];
