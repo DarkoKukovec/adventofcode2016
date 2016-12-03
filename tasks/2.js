e0146ab74e04706a0d4d1cd6cbca2aa5131e32f9
Darko Kukovec <darko@kukovec.eu>
2016-12-03 17:03:46 +0100

    Solution 2/B

--- tasks/2.js
+++ tasks/2.js
@@ -37,5 +37,38 @@ module.exports = {
     // Transform it onto a string
     join('')
   ],
-  b: []
+  b: [
+    // Split the keypres steps
+    split(/\n/),
+
+    // Map the moves to diff values
+    map((item) => item.split('')),
+
+    // Map the keys
+    reduce((res, steps) => {
+      return [...res, steps.reduce((pos, step) => {
+        return {
+          1: {D: 3},
+          2: {R: 3, D: 6},
+          3: {U: 1, R: 4, D: 7, L: 2},
+          4: {D: 8, L: 3},
+          5: {R: 6},
+          6: {U: 2, R: 7, D: 'A', L: 5},
+          7: {U: 3, R: 8, D: 'B', L: 6},
+          8: {U: 4, R: 9, D: 'C', L: 7},
+          9: {L: 8},
+          A: {U: 6, R: 'B'},
+          B: {U: 7, R: 'C', D: 'D', L: 'A'},
+          C: {U: 8, L: 'B'},
+          D: {U: 'B'}
+        }[pos][step] || pos;
+      }, res[res.length - 1])];
+    }, [5]),
+
+    // Remove the first placeholder
+    tail,
+
+    // Transform it onto a string
+    join('')
+  ]
 };
--- tasks/2.test.js
+++ tasks/2.test.js
@@ -19,7 +19,22 @@ module.exports = {
     ]
   },
   b: {
-    input: '',
-    steps: []
+    input: 'ULL\nRRDDD\nLURDL\nUUUUD',
+    steps: [
+      ['ULL', 'RRDDD', 'LURDL', 'UUUUD'],
+
+      [
+        ['U', 'L', 'L'],
+        ['R', 'R', 'D', 'D', 'D'],
+        ['L', 'U', 'R', 'D', 'L'],
+        ['U', 'U', 'U', 'U', 'D']
+      ],
+
+      [5, 5, 'D', 'B', 3],
+
+      [5, 'D', 'B', 3],
+
+      '5DB3'
+    ]
   }
 }
